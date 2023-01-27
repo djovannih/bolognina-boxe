6ceba73c38ed5c60a51c313b767b87f16a7eecf3
Giovanni Improta <bigbossgion@gmail.com>
2023-01-27 13:14:07 +0100

    Don't specify sanity api version

--- utils/sanityClient.ts
+++ utils/sanityClient.ts
@@ -3,6 +3,5 @@ import { createClient } from 'next-sanity'
 export default createClient({
   projectId: process.env.SANITY_API_PROJECT_ID,
   dataset: process.env.SANITY_API_DATASET,
-  apiVersion: process.env.SANITY_API_READ_TOKEN,
   useCdn: process.env.VERCEL_ENV === 'production',
 })
