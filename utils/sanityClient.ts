67c394746c03c0c6e25f706d6fbed87f0376f7b7
Giovanni Improta <bigbossgion@gmail.com>
2023-01-27 13:10:44 +0100

    Use correct env variables also in sanityClient.ts

--- utils/sanityClient.ts
+++ utils/sanityClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity'
 
 export default createClient({
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-  dataset: process.env.SANITY_STUDIO_DATASET,
-  apiVersion: process.env.SANITY_STUDIO_API_VERSION,
-  useCdn: process.env.NODE_ENV === 'production',
+  projectId: process.env.SANITY_API_PROJECT_ID,
+  dataset: process.env.SANITY_API_DATASET,
+  apiVersion: process.env.SANITY_API_READ_TOKEN,
+  useCdn: process.env.VERCEL_ENV === 'production',
 })
