7599b7cff34fcffa512e404f01a9bdd8da19fa20
Giovanni Improta <bigbossgion@gmail.com>
2023-01-27 13:07:09 +0100

    Fix env variables in sanity.cli

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,13 +1,13 @@
 import { loadEnvConfig } from '@next/env'
 import { defineCliConfig } from 'sanity/cli'
 
-const dev = import.meta.env.PROD
+const dev = process.env.VERCEL_ENV === 'development'
 loadEnvConfig(__dirname, dev, { info: () => null, error: console.error })
 
 // @TODO report top-level await bug
 // Using a dynamic import here as `loadEnvConfig` needs to run before this file is loaded
 // const { projectId, dataset } = await import('lib/sanity.api')
-const projectId = import.meta.env.SANITY_STUDIO_PROJECT_ID
-const dataset = import.meta.env.SANITY_STUDIO_DATASET
+const projectId = process.env.SANITY_API_PROJECT_ID
+const dataset = process.env.SANITY_API_DATASET
 
 export default defineCliConfig({ api: { projectId, dataset } })
