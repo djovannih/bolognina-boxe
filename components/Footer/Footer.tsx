b70c10eca514e7641b74c56d61c0a3b6db823870
Giovanni Improta <bigbossgion@gmail.com>
2023-02-21 18:01:08 +0100

    Add some top margin to footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="relative z-20 pb-4 text-gray-200 bg-neutral-900">
+    <footer className="relative z-20 pb-4 mt-8 text-gray-200 bg-neutral-900">
       <div className="max-w-5xl px-4 mx-auto divide-y divide-gray-900 xl:max-w-7xl sm:px-6 md:px-8">
         <div className="flex flex-col-reverse justify-between pt-5 pb-4 bg-top border-t border-neutral-300 lg:flex-row">
           <ul className="flex flex-col space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
