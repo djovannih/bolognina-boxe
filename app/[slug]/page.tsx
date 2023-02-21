535bb3b146fd7801ed717e236172e3dd315efd71
Giovanni Improta <bigbossgion@gmail.com>
2023-02-21 17:58:36 +0100

    Handle 404 pages

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { PortableText } from '@portabletext/react'
 import { Image as SanityImage } from '@sanity/types'
 import Image from 'next/image'
+import { notFound } from 'next/navigation'
 import { groq } from 'next-sanity'
 import { urlForImage } from 'utils/imageBuilder'
 import sanityClient from 'utils/sanityClient'
@@ -27,17 +28,17 @@ const getPage = async (slug: string): Promise<any> =>
 export default async function Page(context: any) {
   const page = await getPage(context.params.slug)
 
-  return (
-    page && (
-      <div className="z-20 mt-16 min-h-[85vh] text-white py-1">
-        <article className="container max-w-5xl px-4 mx-auto divide-gray-900 xl:max-w-7xl sm:px-6 md:px-8 prose-p:my-2">
-          <h1 className="mt-2 mb-6 text-4xl font-medium text-white">
-            {page.title}
-          </h1>
-          <PortableText value={page.content} components={ptComponents} />
-        </article>
-      </div>
-    )
+  return page ? (
+    <div className="z-20 mt-16 min-h-[85vh] text-white py-1">
+      <article className="container max-w-5xl px-4 mx-auto divide-gray-900 xl:max-w-7xl sm:px-6 md:px-8 prose-p:my-2">
+        <h1 className="mt-2 mb-6 text-4xl font-medium text-white">
+          {page.title}
+        </h1>
+        <PortableText value={page.content} components={ptComponents} />
+      </article>
+    </div>
+  ) : (
+    notFound()
   )
 }
 
